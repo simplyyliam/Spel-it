bf703ac64d1e3f95daf95f16f1de001bca143a12
simplyyliam <simplyliam.dev@gmail.com>
2025-05-21 21:07:47 +0200

    fix: Nav not being resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/navigation/Nav";
+
 import { ThemeProvider } from "next-themes";
+import Nav from "@/components/navigation/Nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
